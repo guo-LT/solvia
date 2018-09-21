be22cb5dddd367ada6e23fe6b5064de5f26ddfcc
guoLT <1204807778@qq.com>
2018-09-21 15:09:23 +0800

    solvia

--- js/miaopu.js
+++ js/miaopu.js
@@ -5,40 +5,52 @@
        
          $.get('http://cirray.com:8081/miaopu/huozhiwu/'  ,function (data) {
         console.log(data);
+        $('.collection').append('<li class="collection-item avata row" id="title"><span class="col s2">NO</span><span class="col s2">name</span><span class="col s2">collector</span><span class="col s2">date</span><span class="col s2">remark</span><span class="col s2">total</span></li>');
         data.forEach(function (item, index) {
-            $('.collection').append('<li class="collection-item avatar"><span class="NO"><b>' + item["NO"] + '</b></span><span class="title">' + item["chinese"] + '</span><span class="collector right">' + item["collector"] + '</span><div class="item"><span>' + item["date"] + '</span><span>' + item["additional"] + '</span><span class="right">总数：' + item["total"] + '</span></div></li>');
-        });
-        });
+          
+                  $('.collection').append('<li class="collection-item avata body row"><span class="col s2">'+item["NO"]+'</span><span class="col s2">'+item["chinese"]+'</span><span class="col s2">'+item["collector"]+'</span><span class="col s2">'+item["date"]+'</span><span class="col s2">'+item["additional"]+'</span><span class="col s2" id="data">'+item["total"]+'</span></li>');
+                         })
+                        })
+      
        
      
       $("#2").click(function () {
         $('.collection').empty();
         $.get('http://cirray.com:8081/miaopu/zhongzi/'  ,function (data) {
        console.log(data);
-       data.forEach(function (item, index) {
-         $('.collection').append('<li class="collection-item avatar"><span class="NO">' + item["NO"] + '</span><span>种子数：' + item["total"] + '</span><span class="title right"><b>' + item["chinese"] + '</b></span><div class="item"><div class="item"><span>' + item["date"] + '</span><span class="collector right">' + item["collector"] + '</span></div></li>');
-       });
-       });
+       $('.collection').append('<li class="collection-item avata row"><span class="col s2">NO</span><span class="col s2">name</span><span class="col s3">collector</span><span class="col s2">date</span><span class="col s2">total</span></li>');
+        data.forEach(function (item, index) {
+          
+                  $('.collection').append('<li class="collection-item avata body row"><span class="col s2">'+item["NO"]+'</span><span class="col s2">'+item["chinese"]+'</span><span class="col s3">'+item["collector"]+'</span><span class="col s2">'+item["date"]+'</span><span class="col s2">'+item["total"]+'</span></li>');
+                         })
+                        })
       
-     });
+     
      $("#3").click(function () {
         $('.collection').empty();
         $.get('http://cirray.com:8081/miaopu/huangqinshu/'  ,function (data) {
        console.log(data);
+       $('.collection').append('<li class="collection-item avata row"><span class="col s2">NO</span><span class="col s2">name</span><span class="col s2">collector</span><span class="col s2">date</span><span class="col s2">family</span><span class="col s2">number</span></li>');
        data.forEach(function (item, index) {
-         $('.collection').append('<li class="collection-item avatar "><span class="NO">' + item["NO"] + '</span><span class"family"><b>' + item["family"]+item["genus"] + '</b></span><span class="name right">' + item["species"] + '</span><div class="item"><span>' + item["date"] + '</span><span>活体数：' + item["number"] + '</span><span class="collector right">' + item["collector"] + '</span></div></li>');
-       });
-       });
+         
+                 $('.collection').append('<li class="collection-item avata body row"><span class="col s2">'+item["NO"]+'</span><span class="col s2">'+item["species"]+'</span><span class="col s2">'+item["collector"]+'</span><span class="col s2">'+item["date"]+'</span><span class="col s2">'+item["family"]+'</span><span class="col s2">'+item["number"]+'</span></li>');
+                        })
+                       })
       
      });
      $("#1").click(function () {
         $('.collection').empty();
          $.get('http://cirray.com:8081/miaopu/huozhiwu/'  ,function (data) {
         console.log(data);
+        $('.collection').append('<li class="collection-item avata row" id="title"><span class="col s2">NO</span><span class="col s2">name</span><span class="col s2">collector</span><span class="col s2">date</span><span class="col s2">remark</span><span class="col s2">number</span></li>');
         data.forEach(function (item, index) {
-            $('.collection').append('<li class="collection-item avatar"><span class="NO"><b>' + item["NO"] + '</b></span><span class="title">' + item["chinese"] + '</span><span class="collector right">' + item["collector"] + '</span><div class="item"><span>' + item["date"] + '</span><span>' + item["additional"] + '</span><span class="right">总数：' + item["total"] + '</span></div></li>');
-        });
-        });
+          
+                  $('.collection').append('<li class="collection-item avata body row"><span class="col s2">'+item["NO"]+'</span><span class="col s2">'+item["chinese"]+'</span><span class="col s2">'+item["collector"]+'</span><span class="col s2">'+item["date"]+'</span><span class="col s2">'+item["additional"]+'</span><span class="col s2" id="data">'+item["number"]+'</span>');
+                         })
+                        })
        
       })
     })
+  })
+  
+
